b80bfe9d5c7a966eddf57806203361f27d55730e
Petar <57235791+xamionex@users.noreply.github.com>
2026-09-25 05:34:38 +0200

    bump ver

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,5 +1,5 @@
 internal static class PluginInfo {
 public const string PluginGuid = "amione.InfiniteSave";
 public const string PluginName = "InfiniteSave";
-public const string PluginVersion = "1.0.0";
+public const string PluginVersion = "1.0.1";
 }
